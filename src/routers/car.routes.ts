8988e976f58f5c35636f37659f26b1331c189f65
Filipe Lucena <filipelucena1@gmail.com>
2023-04-12 11:40:20 -0300

    fix: adding middlewares on cars routes

--- src/routers/car.routes.ts
+++ src/routers/car.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { ensureAuthMiddleware, ensureIsValidData } from "../middlewares";
 import { carRequestSchema } from "../schemas/cars";
 import { createCarController, deleteCarController, listCarsController } from "../controllers/cars";
+import { ensureIsValidId } from "../middlewares/ensureIsValidId.middleware";
+import { carRepository } from "../repositories";
 
 const carRoutes = Router();
 
@@ -14,10 +16,10 @@ carRoutes.post(
 
 carRoutes.get("/", listCarsController);
 
-carRoutes.get("/:id");
+carRoutes.get("/:id", ensureIsValidId(carRepository));
 
-carRoutes.patch("/:id");
+carRoutes.patch("/:id", ensureAuthMiddleware, ensureIsValidId(carRepository));
 
-carRoutes.delete("/:id", ensureAuthMiddleware, deleteCarController);
+carRoutes.delete("/:id", ensureAuthMiddleware, ensureIsValidId(carRepository), deleteCarController);
 
 export default carRoutes;
