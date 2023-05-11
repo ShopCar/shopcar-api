ee032f0c8de2ace3543928fc2fcca6bbc1874dbd
Filipe Lucena <filipelucena1@gmail.com>
2023-05-11 07:46:34 -0300

    feat: user relations on retrieveCar route

--- src/services/cars/retrieveCar.service.ts
+++ src/services/cars/retrieveCar.service.ts
@@ -3,7 +3,7 @@ import { carRepository } from "../../repositories";
 const retrieveCarService = async (carId: string) => {
     const car = await carRepository.findOne({
         where: { id: carId },
-        relations: {images: true}
+        relations: {images: true, user: true}
     });
 
     return car;
